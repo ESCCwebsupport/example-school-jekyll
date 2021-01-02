b50e0f1d8b87597bb4985077a201ff894def8e12
Matt Domm <mattdomm@gmail.com>
2021-01-02 12:54:04 +0000

    calendar now says which calendar it uses

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -24,13 +24,27 @@ document.addEventListener('DOMContentLoaded', function () {
         contentHeight: 'auto',
         eventSources: [
             {
-                googleCalendarId: '2g2ibtamvviu6jctgl6j6su340@group.calendar.google.com',
+                calendarName: 'Year 3',
+                googleCalendarId: 'nh1400oaenr3pbpbo4i1im8soc@group.calendar.google.com',
                 color: 'var(--fc-bg-colour-1)',
                 textColor: '#000',
             },
             {
+                calendarName: 'Year 4',
+                googleCalendarId: '7qp5jqum38echhbhoa7i3mbam8@group.calendar.google.com',
+                color: 'var(--fc-bg-colour-3)',
+                textColor: '#000',
+            },
+            {
+                calendarName: 'Whole school',
+                googleCalendarId: 'dv19jja14njutp95722rmkk958@group.calendar.google.com',
+                color: 'var(--fc-bg-colour-2)',
+                textColor: '#000',
+            },
+            {
+                calendarName: 'UK Holidays',
                 googleCalendarId: 'en.uk#holiday@group.v.calendar.google.com'
-            }
+            },
         ]
         ,
         views: {
@@ -96,7 +110,6 @@ document.addEventListener('DOMContentLoaded', function () {
         eventClick: function (info, jsEvent, view) {
             info.jsEvent.preventDefault();
             $('#modalTitle').html(info.event.title);
-            $('#eventDescription').html(info.event.description);
             let startDate = FullCalendarMoment.toMoment(info.event.start, calendar).format("dddd D MMMM YYYY");
             let startTime = FullCalendarMoment.toMoment(info.event.start, calendar).format(", HH:mm");
             let endDate = FullCalendarMoment.toMoment(info.event.end, calendar).format("dddd D MMMM YYYY");
@@ -106,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 isAllDay = "All day";
                 startTime = "";
                 endTime = "";
-                endDate = moment(endDate).subtract(1, 'minutes').format("dddd D MMMM YYYY");
+                endDate = FullCalendarMoment.toMoment(info.event.end, calendar).subtract(1, 'minutes').format("dddd D MMMM YYYY");
                 if (endDate === startDate) {
                     endDate = "";
                 }
@@ -114,17 +127,31 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             else {
                 if (endDate !== startDate) {
-                    endDate = FullCalendarMoment.toMoment(info.event.end, calendar).format("to dddd D MMMM YYYY");
+                    endDate = FullCalendarMoment.toMoment(info.event.end, calendar).format(" to dddd D MMMM YYYY");
                 }
+                else {
                 endDate = "";
-                startTime = FullCalendarMoment.toMoment(info.event.start, calendar).format(" - HH:mm");
-                endTime = " until " + FullCalendarMoment.toMoment(info.event.end, calendar).format("HH:mm");
+                }
+                startTime = FullCalendarMoment.toMoment(info.event.start, calendar).format("h:mma");
+                endTime = " to " + FullCalendarMoment.toMoment(info.event.end, calendar).format("h:mma");
             }
 
-            let eventTimeString = startDate + startTime + endDate + endTime;
+            let eventDateString = `<span class="fas fa-calendar-day"></span>` + startDate + endDate;
+            let eventTimeString = `<span class="far fa-clock"></span>` + startTime + endTime;
+            let description = "";
+            if (info.event.allDay === true) {
+                eventTimeString = `<span class="far fa-clock"></span>` + isAllDay;
+            }
+            if (info.event.extendedProps.description !== undefined) {
+                description = `<span class="fas fa-info-circle"></span>` + info.event.extendedProps.description;
+                $('#eventDescription').show();
+            }
+            else $('#eventDescription').hide();
+
+        $('#calendarName').html(`<span class="fas fa-chalkboard-teacher"></span>` + info.event.source.internalEventSource.extendedProps.calendarName);
+        $('#eventDate').html(eventDateString);
         $('#eventTime').html(eventTimeString);
-        $('#eventIsAllDay').html(isAllDay);
-        // $('#eventUrl').attr('href', info.event.url);
+        $('#eventDescription').html(description);
         $('#fullCalModal').modal();
         }
 
